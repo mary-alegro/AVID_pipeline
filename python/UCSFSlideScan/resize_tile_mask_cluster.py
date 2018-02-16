f6aa85ca7e5c98194c9a9d5452c32b7cab1f31e5
Maryana <maryana@lsi.usp.br>
2018-02-15 16:21:50 -0800

    ...

--- python/UCSFSlideScan/resize_tile_mask_cluster.py
+++ python/UCSFSlideScan/resize_tile_mask_cluster.py
@@ -16,42 +16,43 @@ PIX_5MM = 4095 #5mm = 4095 pixels
 
 def get_mask_info(root_dir):
     file_list = dict()
+    mask_list = dict()
 
     for root, dir, files in os.walk(root_dir):
         if fnmatch.fnmatch(root,'*/RES(*'): #it's inside /RES*
-            for fn in fnmatch.filter(files,'*_*_*.tif'): #find full resolution images
+            for fn in fnmatch.filter(files,'*_*_*.tif'): #get only full resolution images
                 if fn.find('res10') == 0: #skip res10 images
                     continue
                 file_name = os.path.join(root,fn)
                 tiff = tifffile.TiffFile(file_name) #load tiff header only
                 size = tiff.series[0].shape
-                del tiff
+                # compute tile grid.
+                # note that there's always a rounding problem since image size are hardly ever multiples of PIX_5MM
                 nB_rows = size[0] / PIX_5MM  # num. of 5mm high blocks along the 'row' dimension
                 nB_cols = size[1] / PIX_5MM  # num. of 5mm wide blocks along the 'columns' dimension
+                file_list[file_name] = {'home':root, 'size':size, 'tile_grid':[nB_rows, nB_cols]}
+                del tiff
 
-                #get masks folder
-                path_list = root.split(os.sep)
-                nW = len(path_list)
-                mask_dir = ''
-                for d in range(nW-2):
-                    mask_dir+=path_list[d]+os.sep
-                mask_dir = os.path.join(mask_dir,'mask/final_mask')
+    for f in file_list.keys():
+        #get masks folder
+        home_dir = file_list[f]['home']
+        path_list = home_dir.split(os.sep)
+        nW = len(path_list)
+        mask_dir = ''
+        for d in range(nW-2):
+            mask_dir+=path_list[d]+os.sep
+        mask_dir = os.path.join(mask_dir,'mask/final_mask')
+        if os.path.exists(mask_dir):
+            mask_file = ''
+            for root, dir, files in os.walk(mask_dir):
+                for fm in fnmatch.filter(files, '*mask.tif'):  # there should be only one
+                    if fm.find('brain') != -1 or fm.find('wm') != -1:
+                        continue
+                    mask_file = os.path.join(root,fm)
 
-                #check if exists
-                if os.path.exists(mask_dir):
-                    #find mask file name
-                    mask_file = ''
-                    for root, dir, files in os.walk(root_dir):
-                        for fm in fnmatch.filter(files, '*mask.tif'):  # there should be only one mask file
-                            mask_file = os.path.join(root,fm)
+            mask_list[mask_file] = {'home':root, 'full_size':file_list[f]['size'], 'tile_grid':file_list[f]['tile_grid']}
 
-                    if mask_file != '':
-                        file_list[mask_file] = {'home':root, 'full_size':size, 'tile_grid':[nB_rows, nB_cols]}
-
-                else:
-                    continue
-
-    return file_list
+    return mask_list
 
 
 
@@ -59,11 +60,10 @@ def save_metadata(img_name,info_dic,log_file):
 
     tiles = info_dic['tile_grid']
     tile_info = {'name':'Tiles','attrib':{'grid_rows':str(tiles[0]),'grid_cols':str(tiles[1])}}
-    s = info_dic['size']
+    s = info_dic['full_size']
     img_info = {'name':'Image', 'attrib':{'rows':str(s[0]), 'cols':str(s[0]), 'file':img_name, 'home':info_dic['home'], 'children':[tile_info]}}
 
     XMLUtils.dict2xmlfile(img_info,log_file)
-
 
 
 def process_masks(root_dir):
@@ -73,7 +73,6 @@ def process_masks(root_dir):
     TMP_DIR = os.path.join(root_dir,'magick_tmp')
     if not os.path.exists(TMP_DIR):
         os.mkdir(TMP_DIR,0777)
-
     #export Image Magick env variables
     os.environ['MAGICK_TMPDIR'] = TMP_DIR
     os.environ['MAGICK_MEMORY_LIMIT'] = '64Gb'
@@ -85,10 +84,11 @@ def process_masks(root_dir):
 
         fdic = file_dic[fi]
         home_dir = fdic['home']
+        full_size = fdic['full_size']
         tile_grid = fdic['tile_grid']
 
         # Check if file was already processed. If so, skip it.
-        if os.path.exists(os.path.join(home_dir, 'mask/final_mask/tiling_info.xml')):
+        if os.path.exists(os.path.join(home_dir, 'mask_tiling_info.xml')):
             print('File {} has already been tiled. Nothing to do.'.format(fi))
             continue
 
@@ -97,24 +97,42 @@ def process_masks(root_dir):
         if not os.path.exists(tiles_dir):
             os.mkdir(tiles_dir, 0777)
 
+        #
+        # Resize
+        #
+        log_out_name1 = os.path.join(home_dir,'resize_stdout_log.txt')
+        log_err_name1 = os.path.join(home_dir,'resize_stderr_log.txt')
+        log_out1 = open(log_out_name1,'wb+')
+        log_err1 = open(log_err_name1,'wb+')
+        size_str = '{}x{}'.format(full_size[1],full_size[0]) #image magick uses cols x rows convention
 
+        filename = os.path.splitext(os.path.basename(fi))[0]
+        str_rname = os.path.join(home_dir,filename+'_full_res.tif')
+
+        # run resize system process
+        print("Resizing file {}".format(fi))
+        status = subprocess.call(['convert', '-resize', size_str, fi, str_rname], env=dict(os.environ), stderr=log_err1, stdout=log_out1)
+
+
+        #
+        # Tile
+        #
+        log_out_name2 = os.path.join(home_dir,'tile_stdout_log.txt')
+        log_err_name2 = os.path.join(home_dir,'tile_stderr_log.txt')
+        log_out2 = open(log_out_name2,'wb+')
+        log_err2 = open(log_err_name2,'wb+')
         str_tile = '{}x{}@'.format(tile_grid[1],tile_grid[0]) #image magick works with COLSxROWS format
         str_tname = 'tile_%04d.tif' #tile file name pattern
         str_tname = os.path.join(tiles_dir,str_tname)
 
-
-        log_out_name = os.path.join(home_dir,'stdout_log.txt')
-        log_err_name = os.path.join(home_dir,'stderr_log.txt')
-        log_out = open(log_out_name,'wb+')
-        log_err = open(log_err_name,'wb+')
-        # run system process
-
-        print("Tiling file {}".format(fi))
-        status = subprocess.call(['convert', '-crop', str_tile, '+repage', '+adjoin', fi, str_tname], env=dict(os.environ), stderr=log_err, stdout=log_out)
+        #run tiling process
+        print("Tiling file {}".format(str_rname))
+        status = subprocess.call(['convert', '-crop', str_tile, '+repage', '+adjoin', str_rname, str_tname],
+                                 env=dict(os.environ), stderr=log_err2, stdout=log_out2)
 
         #save metadata (used by export_heatmap_metadata.py)
-        meta_file = os.path.join(tiles_dir,'tiling_info.xml')
-        save_metadata(fi,fdic,meta_file)
+        meta_file = os.path.join(tiles_dir,'mask_tiling_info.xml')
+        save_metadata(str_rname,fdic,meta_file)
 
 
 def main():
