f4135a2a323573a791121bfe784ea22a43b37b3e
mary-alegro <maryana@usp.br>
2018-03-09 08:44:36 -0800

    ...

--- python/UCSFSlideScan/seg_tau.py
+++
@@ -1,8 +0,0 @@
-import sys
-import skimage.io as io
-from skimage import color
-from skimage import filters as filt
-from skimage import morphology as morph
-import mahotas
-import matplotlib.pyplot as plt
-
