99feb7fe84a7962c4549d4be2ac3c99ca3c0f915
Maryana <maryana@lsi.usp.br>
2018-04-09 17:10:55 -0700

    ...

---
+++ python/UCSFSlideScan/scripts/run_pipeline_full.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+if [ "$#" -ne 2 ]; then
+	echo "Usage: run_pipeline_full.sh <ROOT_DIR> <CONFIG_PATH>"
+	exit 0  
+fi
+
+ROOT_DIR=$1
+CONF_FILE=$2
+
+echo $ROOT_DIR
+echo $CONF_FILE
+
+python /home/LargeSlideScan/python/UCSFSlideScan/pipeline/run_pipeline.py $ROOT_DIR $CONF_FILE
+
