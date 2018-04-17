22569665a50ebc9b0df6b6df9f6614ab2ec87962
mary-alegro <maryana@usp.br>
2018-04-17 21:56:50 +0000

    ...

--- python/UCSFSlideScan/scripts/test_tiling2.sh
+++ python/UCSFSlideScan/scripts/test_tiling2.sh
@@ -12,7 +12,7 @@ echo 'Converting image to mpc'
 convert /data/000000_-85480_000000.tif /data/tmp.mpc
 
 echo 'Tiling image'
-convert -debug all /data/tmp.mpc -crop 7x20@ +repage +adjoin /data/tiles/tile_%04d.tif
+convert -debug all /data/tmp.mpc -crop 17x20@ +repage +adjoin /data/tiles/tile_%04d.tif
 
 echo 'Deleting temporary files'
 rm /data/tmp.mpc
