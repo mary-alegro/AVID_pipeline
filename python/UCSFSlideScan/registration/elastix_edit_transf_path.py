bd5dadac854b7cd4654ede17b9f468cac263b9dd
mary-alegro <maryana@usp.br>
2018-07-10 13:20:23 -0700

    ...

--- python/UCSFSlideScan/registration/elastix_edit_transf_path.py
+++ python/UCSFSlideScan/registration/elastix_edit_transf_path.py
@@ -1,30 +1,59 @@
 import sys
+import os
+import shutil
+import re
+import glob
 
 
 TAG_STR = 'InitialTransformParametersFileName'
 NEW_TAG_STR = '(InitialTransformParametersFileName "{}/TransformParameters.{}.txt")'
+NO_TFORM_STR = 'NoInitialTransform'
+TFORM_FILE_STR = 'TransformParameters.?.txt'
 
 #(InitialTransformParametersFileName "/home/ssatrawada/Desktop/AV2AT100Registrations/276/1stStep:Automatic/TransformParameters.0.txt")
 
-def edit_txt(file_arr,new_path):
+def edit_txt(file_name,new_path):
+    bkp_file_name = file_name+'.bkp'
+    tmp_file_name = file_name+'.tmp'
 
+    #get file tranfs number
+    basename = os.path.basename(file_name)
+    idx = []
+    for r in re.finditer('\.',basename):
+        idx.append(r.start())
+    file_num = basename[idx[0]+1:idx[1]]
 
+    shutil.copyfile(file_name,bkp_file_name)
+    with open(file_name,'r') as txt:
+        with open(tmp_file_name,'w+') as tmp:
+            for line in txt:
+                if line.find(TAG_STR) > -1:
+                    if line.find(NO_TFORM_STR) <= -1:
+                        line = NEW_TAG_STR.format(new_path,file_num)+'\n'
+                tmp.write(line)
+                tmp.flush()
+
+    shutil.move(tmp_file_name,file_name)
+
+def get_tform_list(tform_dir):
+    tform_list = glob.glob(os.path.join(tform_dir,TFORM_FILE_STR))
+    return tform_list
+
+def change_tform_files(tform_dir,new_path):
+    file_list = get_tform_list(tform_dir)
+    for file_name in file_list:
+        print('Editing file {}'.format(file_name))
+        edit_txt(file_name,new_path)
 
 
 def main():
-    if len(sys.argv) != 5:
-        print('Usage: apply_combined_registrations.py <slice_dir> <mov_file> <ref_file> <reg_file>')
+    if len(sys.argv) != 3:
+        print('Usage: elastix_edit_transf_path.py <transf_files_dir> <new_path>')
         exit()
 
-    slice_dir = sys.argv[1]
-    mov_file = sys.argv[2] #histology or heatmap
-    ref_file = sys.argv[3] #blockface
-    reg_file = sys.argv[4] #output: registered heatmap/histology
-
-    # slice_dir = '/home/maryana/storage/Posdoc/AVID/AV23/AT100/full_res/AT100_164'
-    # mov_file = '/home/maryana/storage/Posdoc/AVID/AV23/AT100/full_res/AT100_164/reg/AT100_164_res10.nii'
-    # ref_file = '/home/maryana/storage/Posdoc/AVID/AV23/AT100/full_res/AT100_164/reg/AV13-002_0164.png.nii'
-    # reg_file = '/home/maryana/storage/Posdoc/AVID/AV23/AT100/full_res/AT100_164/reg/reg.nii'
+    files_dir = sys.argv[1]
+    new_path = sys.argv[2] #histology or heatmap
+    change_tform_files(files_dir,new_path)
 
 
 if __name__ == '__main__':
